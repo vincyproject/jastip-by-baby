396d6704d8fe8cd1f1fadb269575861f7688fcff
malvino48 <malvinocgv@gmail.com>
2025-01-13 19:36:23 +0700

    Revisi detail product

--- frontend/src/components/OurPolicy.jsx
+++ frontend/src/components/OurPolicy.jsx
@@ -7,18 +7,20 @@ const OurPolicy = () => {
       
       <div>
         <img src={assets.exchange_icon} className='w-12 m-auto mb-5' alt="" />
-        <p className=' font-semibold'>Easy Exchange Policy</p>
-        <p className=' text-gray-400'>We offer hassle free  exchange policy</p>
+        <p className=' font-semibold'>Easily to Shop</p>
+        <p className=' text-gray-400'>We have offline Store</p>
       </div>
       <div>
         <img src={assets.quality_icon} className='w-12 m-auto mb-5' alt="" />
-        <p className=' font-semibold'>7 Days Return Policy</p>
-        <p className=' text-gray-400'>We provide 7 days free return policy</p>
+        <p className=' font-semibold'>Easily to Payment</p>
+        <p className=' text-gray-400'>We provide many types</p>
+        <p className=' text-gray-400'>of payment methods</p>
       </div>
       <div>
         <img src={assets.support_img} className='w-12 m-auto mb-5' alt="" />
         <p className=' font-semibold'>Best customer support</p>
-        <p className=' text-gray-400'>we provide 24/7 customer support</p>
+        <p className=' text-gray-400'>We provide 24/7</p>
+        <p className=' text-gray-400'>customer support</p>
       </div>
 
     </div>
